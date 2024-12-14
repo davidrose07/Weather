a0111b6cd6c1317c06bf658ee266189af7bc09d2
davidrose07 <davidrose071783@gmail.com>
2024-12-14 09:38:31 -0600

    Added init file

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     python_requires=">=3.6",
     entry_points={
         "console_scripts": [
-            "weather=weather.main:main",
+            "weather=main:main",
         ],
     },
 )
